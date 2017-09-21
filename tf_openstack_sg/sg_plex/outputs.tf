4121da9d723b41fa0578fa1e115b0f4f17e7188c
AndrewCsoka <andrew.csoka@basekit.com>
2017-09-21 16:01:50 +0100

    Use openstack sg, not aws!

--- tf_openstack_sg/sg_plex/outputs.tf
+++ tf_openstack_sg/sg_plex/outputs.tf
@@ -1,4 +1,4 @@
 // Output ID of sg_plex Security Group we made
 output "security_group_id_plex" {
-  value = "${aws_security_group.main_security_group.id}"
+  value = "${openstack_compute_secgroup_v2.main_security_group.id}"
 }
