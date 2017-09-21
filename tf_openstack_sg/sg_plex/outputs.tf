a24701d12cfe028fd689d11e53691560d147ffa2
AndrewCsoka <andrew.csoka@basekit.com>
2017-09-21 16:23:52 +0100

    Fix the output

--- tf_openstack_sg/sg_plex/outputs.tf
+++ tf_openstack_sg/sg_plex/outputs.tf
@@ -1,4 +1,4 @@
 // Output ID of sg_plex Security Group we made
 output "security_group_id_plex" {
-  value = "${openstack_compute_secgroup_v2.main_security_group.id}"
+  value = "${openstack_compute_secgroup_v2.plex_security_group.name}"
 }
